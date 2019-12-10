914c14ad916fe4a666a6ad567ca98232348382b7
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-10 07:41:53 +0000

    Done.

--- src/Components/Episode.js
+++ src/Components/Episode.js
@@ -1,13 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const Episode = (props) => {
-  let { myEpisode } = props
-
-  return (
-    <div>
-      Episode {myEpisode.number} - {myEpisode.name}
-    </div>
-  )
-}
+const Episode = ({ eachEpisode }) => {
+  return <div>Episode {eachEpisode.number} - {eachEpisode.name}</div>;
+};
 
 export default Episode;
